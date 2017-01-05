752e7a33d67054d3e5a49d0596e4723974065b6a
Steve Martin <steven.martin@esure.com>
2017-01-05 14:32:49 +0000

    Increased sleep time.

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -3,6 +3,6 @@ composer create-project
 npm install
 sudo docker-compose up -d
 echo "Initiating Apache and Drupal within the docker container, this will take a few minutes..."
-sleep 30s
+sleep 60s
 sudo docker-compose exec web drush config-set system.site uuid "1a28a7c9-8c3b-4348-a6eb-1cd6ecb7833a"
 sudo docker-compose exec web drush cim
