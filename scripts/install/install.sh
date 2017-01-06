e13a8f6661cbef005e8cc05403339b3261942102
Steve Martin <steven.martin@esure.com>
2017-01-06 09:53:36 +0000

    Install tidy.

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -7,4 +7,4 @@ sleep 60s
 echo "...ok that should be long enough, let's try it now."
 sudo docker-compose exec web drush config-set system.site uuid "1a28a7c9-8c3b-4348-a6eb-1cd6ecb7833a"
 sudo docker-compose exec web drush cim
-
+echo "The Digital App is now available. You will need to visit the homepage to initiate the site and then set the Admin password using the 'set_admin_password.sh' script."
